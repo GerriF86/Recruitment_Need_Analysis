252ee99fcd0593f7ccffbfa17ed75e41e1961de5
GerriF86 <gerrit.fabisch2024@gmail.com>
2024-11-15 12:49:34 +0100

    aufgeräumt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 
 import streamlit as st
+from PIL import Image
 
 # Page Configuration
 st.set_page_config(page_title="Recruitment Need Analysis", page_icon="📄", layout="wide")
 
 # App Title
-st.title("Recruitment Need Analysis Web App")
-st.subheader("Streamline Job Description Creation for HR Professionals")
+st.title("Recruitment Need Analysis App")
+st.subheader("Discover important aspects of the role you want to fill and find out Whats Skills and Qualifications you are really looking for?")
 
 # Introduction
 st.markdown("""
@@ -14,7 +15,10 @@ st.markdown("""
     by guiding them through interactive questions. Answer a series of prompts to 
     generate a formatted job description tailored to your needs.
 """)
-
+# Include an Image on the Front Page
+#image = Image.open('images\iceberg-model-of-system-thinking-is-an-illustration-of-a-blue-mountain-and-presentation-this-theory-is-to-analyze-the-root-causes-of-events-hidden-underwater-for-developing-marketing-and-trend-vector.jpg')
+#st.image(image, caption="Iceberg Model of System Thinking", use_column_width=True)
+    
 # Step 1: Role Selection
 role = st.selectbox(
     "Select the Position:",
@@ -28,16 +32,27 @@ if role == "Data Scientist":
         "Choose Specializations:",
         ["Machine Learning", "Statistics", "Deep Learning", "Data Visualization"]
     )
-    tools = st.text_input("List Tools/Technologies Required (comma-separated):")
+    tools = st.multiselect(
+        "Choose Tools/Technologies:",
+        ["Python", "R", "TensorFlow", "PyTorch", "SQL", "Pandas", "NumPy", "Jupyter Notebooks"]
+    )
 elif role == "Data Analyst":
     focus_area = st.radio(
         "Primary Focus:",
         ["Data Cleaning", "Reporting", "Business Analysis"]
     )
-    software = st.text_input("List Key Software Tools (comma-separated):")
+    software = st.multiselect(
+        "Choose Key Software Tools:",
+        ["Excel", "Power BI", "Tableau", "SQL", "Python", "R", "Google Analytics"]
+    )
+elif role == "Software Engineer":
+    tech_stack = st.multiselect(
+        "Select Preferred Tech Stack:",
+        ["Java", "C++", "Python", "JavaScript", "Ruby", "AWS", "Docker", "Kubernetes"]
+    )
 else:
     custom_details = st.text_area("Describe the Role in Detail:")
-
+    
 # Step 3: Work Model and Compensation
 st.header("Work Environment and Compensation")
 work_model = st.selectbox(
